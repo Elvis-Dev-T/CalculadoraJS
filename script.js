5ad55883f2be528f653487eac71d4d0379e53743
Elvis-Dev-T <rochaelielvis@gmail.com>
2022-04-18 07:47:13 -0300

    CaclculadoraJS

---
+++ script.js
@@ -0,0 +1,49 @@
+//Funções declarativas
+
+/*function iAmAFunction() {
+    console.log("I am a function");
+} iAmAFunction();
+*/
+
+//Expressões de funções - São funções atribuidas a expressões. A nomeação das funções por expressão é opcional;
+
+//Com nomeação
+
+/*
+var funcao = function funcao() {
+    console.log("I am a function");
+} 
+funcao();
+*/ 
+
+//Sem nomeação
+
+/*
+var funcaoOne = function () {
+    console.log("I am a function too!");
+}
+funcaoOne();
+*/
+
+//Arrow functions - You don't call the expression "function"
+
+/*
+var funcaoTwo = () => {
+    console.log("I am a arrow function");
+}
+funcaoTwo();
+*/
+
+//function iAmAFunction() {
+  //  alert("Function " + typeof(iAmAFunction))
+//} iAmAFunction();
+
+//let funcao = function () {
+  //  document.write("I am a function")
+//}; funcao();
+
+//let funcao = () => {
+  //  console.log("Heyy man!");
+//} 
+//funcao();
+
